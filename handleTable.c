eaaba72cb7afc7e80a32335490344404303cf2e5
binabair <sbair@calpoly.edu>
2026-04-25 19:36:08 -0700

    finished addHandleEntry fxn

--- handleTable.c
+++ handleTable.c
@@ -12,7 +12,7 @@
 
 //struct for handle entries
 //make the table
-//add to table - check if resize is necessary 
+//add to table - check if resize is necessary (motherfuckerrrr thatll be two fxns)
 //remove from table
 
 //struct
@@ -31,7 +31,14 @@ typedef struct {
 //make it
 HandleTable createHandleTable(int size) {
     HandleTable currentTable;
+
     currentTable.entries = malloc(size * sizeof(HandleEntry));
+    //initialize to prevent weird stuff
+    for (int i = 0; i < currentTable.capacity; i++){
+        currentTable.entries[i].valid = 0;
+        currentTable.entries[i].socketNum = -1;
+    }
+
     currentTable.capacity = size;
     currentTable.count = 0;
     return currentTable;
@@ -47,8 +54,42 @@ int addHandleEntry(HandleEntry handle, HandleTable *table){
             return -1;
         }
     }
-    //check length
-    if ()
+
+    //check length - if not enough room, call expand and double size
+    if (table->count == table->capacity){
+        int expansion = expandHandleTable(table);
+        if (expansion == -1){
+            perror("realloc failed, sincerely the addHandleEntry fxn");
+        }
+    }
+
+    //put in new entry, increment count and return
+    table->entries[table->count + 1] = handle;
+    table->count += 1;
+
+    return table;
 }
+
+//resize
+int expandHandleTable(HandleTable *table){
+    int newCapacity = table->capacity *2;
+
+    HandleEntry *temp = realloc(table->entries, newCapacity * sizeof(HandleEntry));
+    if (temp == NULL){
+        perror("realloc failed, sincerly the expandHandleTable fxn");
+        return -1;
+    }
+    
+    table->entries = temp;
+
+    //prevent garbage values
+    for (int i = table->capacity; i < newCapacity; i++){
+        table->entries[i].valid = 0;
+        table->entries[i].socketNum = -1;
+    }
+
+    table->capacity = newCapacity;
+    return 0;
+}    
 
 
