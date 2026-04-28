ab16b2854302e230d108327ae231c7aec5e48a21
binabair <sbair@calpoly.edu>
2026-04-27 17:50:47 -0700

    uhhh

--- cclient.c
+++ cclient.c
@@ -24,6 +24,7 @@
 #include "networks.h"
 #include "safeUtil.h"
 #include "sendRecvPDU.h"
+#include "handleTable.h"
 
 #define MAXBUF 1024
 #define DEBUG_FLAG 1
@@ -32,23 +33,6 @@ void sendToServer(int socketNum);
 int readFromStdin(uint8_t * buffer);
 void checkArgs(int argc, char * argv[]);
 
-int main(int argc, char * argv[])
-{
-	int socketNum = 0;         //socket descriptor
-	
-	checkArgs(argc, argv);
-
-	/* set up the TCP Client socket  */
-	socketNum = tcpClientSetup(argv[1], argv[2], DEBUG_FLAG);
-	
-	while(1){
-		sendToServer(socketNum);
-	}
-	
-	close(socketNum);
-	
-	return 0;
-}
 
 void sendToServer(int socketNum)
 {
@@ -85,10 +69,32 @@ void sendToServer(int socketNum)
 	
 }
 
+//%M - send to specific destination, %B - broadcast, %C - send to some, not all,
+//%L - list all handlesknown by server
+
+void MCall(){
+
+}
+
+void BCall(){
+
+}
+
+void CCall(){
+
+}
+
+void LCall(){
+	//
+}
+
+
 int readFromStdin(uint8_t * buffer)
 {
 	char aChar = 0;
-	int inputLen = 0;        
+	int inputLen = 0;  
+	
+	//do print the $: first, then call poll cause poll with take over everything and not let it print
 	
 	// Important you don't input more characters than you have space 
 	buffer[0] = '\0';
@@ -110,7 +116,7 @@ int readFromStdin(uint8_t * buffer)
 	return inputLen;
 }
 
-void checkArgs(int argc, char * argv[])
+void checkArgs(int argc, char * argv[]) 
 {
 	/* check command line arguments  */
 	if (argc != 3)
@@ -118,4 +124,24 @@ void checkArgs(int argc, char * argv[])
 		printf("usage: %s host-name port-number \n", argv[0]);
 		exit(1);
 	}
+}
+
+int main(int argc, char * argv[])
+{
+	int socketNum = 0;         //socket descriptor
+	
+	checkArgs(argc, argv);
+
+	/* set up the TCP Client socket  */
+	socketNum = tcpClientSetup(argv[1], argv[2], DEBUG_FLAG);
+
+	
+	
+	while(1){
+		sendToServer(socketNum);
+	}
+	
+	close(socketNum);
+	
+	return 0;
 }
--- handleTable.h
+++ handleTable.h
@@ -1,2 +1,8 @@
 
+HandleTable createHandleTable(int size);
+int addHandleEntry(char *handleName, int socketNum, HandleTable *table);
+int expandHandleTable(HandleTable *table);
+int removeHandleEntry(HandleEntry handle, HandleTable *table);
+bool lookUpHandle (char * handleName, HandleTable *table);
+bool lookUpSocket (int socketNum, HandleTable *table);
 
